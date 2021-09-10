cc948c509f5259ed4fdf92d344c4448524cb0458
Daniel van der Ploeg <danielvdp56@gmail.com>
2021-09-10 20:39:02 +0930

    Get the number of days between two datetime parameters

--- datetime_api/api.py
+++ datetime_api/api.py
@@ -1,8 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from datetime import datetime
+from dateutil import parser
 
 bp = Blueprint("api", __name__)
 
 
-@bp.route("/")
-def index():
-    return '<h1>Hello World</h1>'
+@bp.route("/days")
+def days():
+    """Get the number of days between two datetime parameters"""
+    date1 = request.args.get("date1")
+    date2 = request.args.get("date2")
+    unit = request.args.get("unit") or "days"
+
+    date = parser.parse(date1), parser.parse(date2)
+    difference = date[1] - date[0]
+    difference = difference.days
+
+    response = {
+        "difference": str(difference),
+        "unit": unit
+    }
+
+    return response
--- tests/test_datetime-api.py
+++ tests/test_datetime-api.py
@@ -12,8 +12,15 @@ def client():
         yield client
 
 
-def test_test(client):
-    """Testing tests."""
+def test_days(client):
+    """Test the correct number of days is returned from two dates"""
+    test_data = [
+        {"date1": "2021-01-01", "date2": "2022-01-01", "result": b'365'},
+        {"date1": "2021-01-01", "date2": "2021-01-31", "result": b'30'},
+        {"date1": "1984-01-01", "date2": "1985-01-01", "result": b'366'},
+        {"date1": "2021-01-01", "date2": "2021-01-01", "result": b'0'},
+    ]
 
-    rv = client.get('/')
-    assert b'Hello World' in rv.data
+    for data in test_data:
+        rv = client.get('/days?date1=' + data.get("date1") + '&date2=' + data.get("date2"))
+        assert data.get("result") in rv.data
