c33997ae68faeae0ea17faf93877eae1cb3fb966
Daniel van der Ploeg <danielvdp56@gmail.com>
2021-09-11 15:48:23 +0930

    Moved date calculations into a class and added functionality for different measures of time
    Refactored the tests into a test class

--- datetime_api/api.py
+++ datetime_api/api.py
@@ -6,6 +6,69 @@ import math
 bp = Blueprint("api", __name__)
 
 
+class DifferenceBetween:
+    def __init__(self, start_date, end_date, unit):
+        self.unit = unit
+
+        self.start_date = parser.parse(start_date)
+        self.end_date = parser.parse(end_date)
+
+        if self.unit == "seconds":
+            self.difference = self.secondsBetween()
+        elif self.unit == "minutes":
+            self.difference = self.minutesBetween()
+        elif self.unit == "hours":
+            self.difference = self.hoursBetween()
+        elif self.unit == "days":
+            self.difference = self.daysBetween()
+        elif self.unit == "years":
+            self.difference = self.yearsBetween()
+
+    def getDifference(self):
+        return self.difference
+
+    def secondsBetween(self):
+        return self.daysBetween() * 86400
+
+    def minutesBetween(self):
+        return self.daysBetween() * 1440
+
+    def hoursBetween(self):
+        return self.daysBetween() * 24
+
+    def daysBetween(self):
+        difference = self.end_date - self.start_date
+        return difference.days
+
+    def weekdaysBetween(self):
+        # Get the number of complete weeks
+        weeks = math.floor(self.daysBetween() / 7)
+        # Use that to calculate the number of weekdays
+        weekdays = weeks * 5
+        # Count the weekdays of the last incomplete week
+        incomplete_week = self.start_date + timedelta(weeks=weeks)
+        while incomplete_week < self.end_date:
+            if incomplete_week.weekday() < 5:
+                weekdays += 1
+            incomplete_week += timedelta(days=1)
+
+        return weekdays
+
+    def completeWeeksBetween(self):
+        # Get the number of complete weeks
+        # if difference is negative don't floor the result
+        difference = self.daysBetween()
+        if difference >= 0:
+            weeks = math.floor(difference / 7)
+        else:
+            weeks = math.ceil(difference / 7)
+
+        return weeks
+
+    def yearsBetween(self):
+        return self.daysBetween() / 365
+
+
 @bp.route("/days")
 def days():
     """Get the number of days between two datetime parameters"""
@@ -13,9 +76,7 @@ def days():
     end_date = request.args.get("end_date")
     unit = request.args.get("unit") or "days"
 
-    date = parser.parse(start_date), parser.parse(end_date)
-    difference = date[1] - date[0]
-    difference = difference.days
+    difference = DifferenceBetween(start_date, end_date, unit).getDifference()
 
     response = {
         "difference": difference,
@@ -32,20 +93,7 @@ def weekdays():
     end_date = request.args.get("end_date")
     unit = request.args.get("unit") or "days"
 
-    date = parser.parse(start_date), parser.parse(end_date)
-    difference = date[1] - date[0]
-    difference = difference.days
-
-    # Get the number of complete weeks
-    weeks = math.floor(difference / 7)
-    # Use that to calculate the number of weekdays
-    weekdays = weeks * 5
-    # Count the weekdays of the last incomplete week
-    incomplete_week = date[0] + timedelta(weeks=weeks)
-    while incomplete_week < date[1]:
-        if incomplete_week.weekday() < 5:
-            weekdays += 1
-        incomplete_week += timedelta(days=1)
+    weekdays = DifferenceBetween(start_date, end_date, unit).weekdaysBetween()
 
     response = {
         "difference": weekdays,
@@ -62,16 +110,7 @@ def completeweeks():
     end_date = request.args.get("end_date")
     unit = request.args.get("unit") or "weeks"
 
-    date = parser.parse(start_date), parser.parse(end_date)
-    difference = date[1] - date[0]
-    difference = difference.days
-
-    # Get the number of complete weeks
-    # if difference is negative don't floor the result
-    if difference >= 0:
-        weeks = math.floor(difference / 7)
-    else:
-        weeks = math.ceil(difference / 7)
+    weeks = DifferenceBetween(start_date, end_date, unit).completeWeeksBetween()
 
     response = {
         "difference": weeks,
--- tests/test_datetime-api.py
+++ tests/test_datetime-api.py
@@ -1,63 +1,93 @@
-import pytest
+import unittest
 
 from datetime_api import create_app
 
 
-@pytest.fixture
-def client():
-    app = create_app()
-    app.config['TESTING'] = True
+class TestAPI(unittest.TestCase):
+    def setUp(self):
+        app = create_app()
+        app.config['TESTING'] = True
+        self.app = app.test_client()
 
-    with app.test_client() as client:
-        yield client
+    def test_days(self):
+        """Test the correct number of days is returned from two dates"""
+        test_data = [
+            {"start_date": "2021-01-01", "end_date": "2022-01-01", "result": b'365'},
+            {"start_date": "2021-01-01", "end_date": "2021-01-02", "result": b'1'},
+            {"start_date": "2021-01-02", "end_date": "2021-01-03", "result": b'1'},
+            {"start_date": "2020-12-31", "end_date": "2021-01-01", "result": b'1'},
+            {"start_date": "2021-01-01", "end_date": "2021-01-31", "result": b'30'},
+            {"start_date": "1984-01-01", "end_date": "1985-01-01", "result": b'366'},
+            {"start_date": "2021-01-01", "end_date": "2021-01-01", "result": b'0'},
+            {"start_date": "2022-01-01", "end_date": "2021-01-01", "result": b'-365'},
+        ]
 
+        for data in test_data:
+            rv = self.app.get('/days?start_date=' + data.get("start_date") + '&end_date=' + data.get("end_date"))
+            assert data.get("result") in rv.data
 
-def test_days(client):
-    """Test the correct number of days is returned from two dates"""
-    test_data = [
-        {"start_date": "2021-01-01", "end_date": "2022-01-01", "result": b'365'},
-        {"start_date": "2021-01-01", "end_date": "2021-01-02", "result": b'1'},
-        {"start_date": "2021-01-02", "end_date": "2021-01-03", "result": b'1'},
-        {"start_date": "2021-01-01", "end_date": "2021-01-31", "result": b'30'},
-        {"start_date": "1984-01-01", "end_date": "1985-01-01", "result": b'366'},
-        {"start_date": "2021-01-01", "end_date": "2021-01-01", "result": b'0'},
-        {"start_date": "2022-01-01", "end_date": "2021-01-01", "result": b'-365'},
-    ]
+    def test_days_units(self):
+        """
+        Test the correct number of seconds, minutes, hours or years is returned
+        from two dates
+        """
+        test_data = [
+            {"start_date": "2021-01-01", "end_date": "2022-01-01", "unit": "seconds", "result": b'31536000'},
+            {"start_date": "2021-01-01", "end_date": "2021-01-02", "unit": "seconds", "result": b'86400'},
+            {"start_date": "1984-01-01", "end_date": "1985-01-01", "unit": "seconds", "result": b'31622400'},
+            {"start_date": "2021-01-01", "end_date": "2021-01-01", "unit": "seconds", "result": b'0'},
+            {"start_date": "2022-01-01", "end_date": "2021-01-01", "unit": "seconds", "result": b'-31536000'},
+            {"start_date": "2021-01-01", "end_date": "2022-01-01", "unit": "minutes", "result": b'525600'},
+            {"start_date": "2021-01-01", "end_date": "2021-01-02", "unit": "minutes", "result": b'1440'},
+            {"start_date": "1984-01-01", "end_date": "1985-01-01", "unit": "minutes", "result": b'527040'},
+            {"start_date": "2021-01-01", "end_date": "2021-01-01", "unit": "minutes", "result": b'0'},
+            {"start_date": "2022-01-01", "end_date": "2021-01-01", "unit": "minutes", "result": b'-525600'},
+            {"start_date": "2021-01-01", "end_date": "2022-01-01", "unit": "hours", "result": b'8760'},
+            {"start_date": "2021-01-01", "end_date": "2021-01-02", "unit": "hours", "result": b'24'},
+            {"start_date": "1984-01-01", "end_date": "1985-01-01", "unit": "hours", "result": b'8784'},
+            {"start_date": "2021-01-01", "end_date": "2021-01-01", "unit": "hours", "result": b'0'},
+            {"start_date": "2022-01-01", "end_date": "2021-01-01", "unit": "hours", "result": b'-8760'},
+            {"start_date": "2021-01-01", "end_date": "2022-01-01", "unit": "years", "result": b'1'},
+            {"start_date": "2021-01-01", "end_date": "2021-01-02", "unit": "years", "result": b'0'},
+            {"start_date": "1984-01-01", "end_date": "1985-01-01", "unit": "years", "result": b'1'},
+            {"start_date": "2021-01-01", "end_date": "2021-01-01", "unit": "years", "result": b'0'},
+            {"start_date": "2022-01-01", "end_date": "2021-01-01", "unit": "years", "result": b'-1'},
+        ]
 
-    for data in test_data:
-        rv = client.get('/days?start_date=' + data.get("start_date") + '&end_date=' + data.get("end_date"))
-        assert data.get("result") in rv.data
+        for data in test_data:
+            rv = self.app.get('/days?start_date=' + data.get("start_date") + '&end_date=' + data.get("end_date")
+                              + '&unit=' + data.get("unit"))
+            assert data.get("result") in rv.data
 
+    def test_weekdays(self):
+        """Test the correct number of weekdays is returned from two dates"""
+        test_data = [
+            {"start_date": "2021-01-01", "end_date": "2022-01-01", "result": b'261'},
+            {"start_date": "2021-01-01", "end_date": "2021-01-02", "result": b'1'},
+            {"start_date": "2021-01-02", "end_date": "2021-01-03", "result": b'0'},
+            {"start_date": "2021-01-01", "end_date": "2021-01-31", "result": b'21'},
+            {"start_date": "1984-01-01", "end_date": "1985-01-01", "result": b'261'},
+            {"start_date": "2021-01-01", "end_date": "2021-01-01", "result": b'0'},
+            {"start_date": "2022-01-01", "end_date": "2021-01-01", "result": b'-261'},
+        ]
 
-def test_weekdays(client):
-    """Test the correct number of weekdays is returned from two dates"""
-    test_data = [
-        {"start_date": "2021-01-01", "end_date": "2022-01-01", "result": b'261'},
-        {"start_date": "2021-01-01", "end_date": "2021-01-02", "result": b'1'},
-        {"start_date": "2021-01-02", "end_date": "2021-01-03", "result": b'0'},
-        {"start_date": "2021-01-01", "end_date": "2021-01-31", "result": b'21'},
-        {"start_date": "1984-01-01", "end_date": "1985-01-01", "result": b'261'},
-        {"start_date": "2021-01-01", "end_date": "2021-01-01", "result": b'0'},
-        {"start_date": "2022-01-01", "end_date": "2021-01-01", "result": b'-261'},
-    ]
+        for data in test_data:
+            rv = self.app.get('/weekdays?start_date=' + data.get("start_date") + '&end_date=' + data.get("end_date"))
+            assert data.get("result") in rv.data
 
-    for data in test_data:
-        rv = client.get('/weekdays?start_date=' + data.get("start_date") + '&end_date=' + data.get("end_date"))
-        assert data.get("result") in rv.data
+    def test_completeweeks(self):
+        """Test the correct number of weekdays is returned from two dates"""
+        test_data = [
+            {"start_date": "2021-01-01", "end_date": "2022-01-01", "result": b'52'},
+            {"start_date": "2021-01-01", "end_date": "2021-01-02", "result": b'0'},
+            {"start_date": "2021-01-02", "end_date": "2021-01-03", "result": b'0'},
+            {"start_date": "2021-01-01", "end_date": "2021-01-31", "result": b'4'},
+            {"start_date": "1984-01-01", "end_date": "1985-01-01", "result": b'52'},
+            {"start_date": "2021-01-01", "end_date": "2021-01-01", "result": b'0'},
+            {"start_date": "2022-01-01", "end_date": "2021-01-01", "result": b'-52'},
+        ]
 
-
-def test_completeweeks(client):
-    """Test the correct number of weekdays is returned from two dates"""
-    test_data = [
-        {"start_date": "2021-01-01", "end_date": "2022-01-01", "result": b'52'},
-        {"start_date": "2021-01-01", "end_date": "2021-01-02", "result": b'0'},
-        {"start_date": "2021-01-02", "end_date": "2021-01-03", "result": b'0'},
-        {"start_date": "2021-01-01", "end_date": "2021-01-31", "result": b'4'},
-        {"start_date": "1984-01-01", "end_date": "1985-01-01", "result": b'52'},
-        {"start_date": "2021-01-01", "end_date": "2021-01-01", "result": b'0'},
-        {"start_date": "2022-01-01", "end_date": "2021-01-01", "result": b'-52'},
-    ]
-
-    for data in test_data:
-        rv = client.get('/completeweeks?start_date=' + data.get("start_date") + '&end_date=' + data.get("end_date"))
-        assert data.get("result") in rv.data
+        for data in test_data:
+            rv = self.app.get(
+                '/completeweeks?start_date=' + data.get("start_date") + '&end_date=' + data.get("end_date"))
+            assert data.get("result") in rv.data
